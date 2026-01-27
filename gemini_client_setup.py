df9f42a733bd984df5c700a5b91b455aa6c7590f
Shiva Shaklya <shivashaklya@gmail.com>
2026-01-27 16:40:01 +0530

    Idea understanding completed

--- gemini_client_setup.py
+++ gemini_client_setup.py
@@ -2,35 +2,24 @@ from google import genai
 from dotenv import load_dotenv
 import os
 import json
-from typing import Dict, Any
+from typing import List, Dict, Any, Optional, Tuple
 
 load_dotenv()
 api_key1=os.getenv("API-KEY")
 client=genai.Client(api_key=api_key1)
 
-'''#
-response=client.models.generate_content_stream(
-    model="gemini-flash-latest",
-    contents="why do the stars twinkle?"
-)
-
-for stream in response:
-    print(stream.text)
-
 #
-chat=client.chats.create(model="gemini-flash-lite-latest")
+def call_llm(prompt_template: str, user_message: str, history_contents: Optional[List[Dict[str,Any]]]=None) -> Tuple[Dict[str,Any], List[Dict[str,Any]]]:
 
-while True:
-    message=input("> ")
-    if message=="exit":
-        break
-    result=chat.send_message(message)
-    print(result.text)'''
-#
-def call_llm(prompt_template: str, user_message= str):
+    contents: List[Dict[str,Any]]=[]
+    if history_contents:
+        contents.extend(history_contents)
+
+    contents.append({"role":"user","parts":[{"text":user_message}]})
+
     response=client.models.generate_content(
         model="gemini-flash-latest",
-        contents=user_message,
+        contents=contents,
         config={
             "system_instruction": prompt_template,
             "response_mime_type":"application/json"
@@ -40,7 +29,8 @@ def call_llm(prompt_template: str, user_message= str):
     text=(response.text or "").strip()
     try:
         data=json.loads(text)
-        return data
+        contents.append({"role":"model","parts":[{"text":text}]})
+        return data,contents
     except:
         raise ValueError("Incorrect output format by LLM. Raw output:\n",text)
     
--- query_processing.py
+++ query_processing.py
@@ -2,55 +2,17 @@ import re
 import spacy
 from collections import Counter
 from gemini_client_setup import call_llm
+from chat_store import create_chat, save_chat, add_turn
+import json
 
+#Create json file per project per user (idea_raw,idea_understanding (domain/subdomain/stage/one-liner + target user (later, dynamically)), finalized = true/false, constraints (geolocation (place), budget (price), B2B/B2C(promotion))
+#report (generated markdown/pdf), report_index (embeddings / chunks for Q&A over the report), conversation_history
+#Get confirmation for domain etc., update db, continue conversation for constraints, finalize and update db, create customer persona dynamically, scrape market overview, create report
 #Get customer persona and market overview from user query
 #Allow cross questioning
+#Ensure database + agentic competitive analysis
 
 nlp = spacy.load("en_core_web_sm")
-
-def idea_confirmation(query, confidence):
-    prompt_template="""You are an expert product and startup analyst.
-    Given:
-    - A processed product idea: {query}
-    - A confidence score indicating specificity: {confidence}
-
-    Classify the ideation maturity of the idea using ONLY one of the following stages:
-    - exploration
-    - problem_framing
-    - solution_ideation
-    - solution_detailing
-    - validation_ready
-
-    Definitions:
-    - exploration: vague idea, no clear product or problem
-    - problem_framing: problem is clear, solution is not
-    - solution_ideation: a solution is proposed but not explained
-    - solution_detailing: a solution with mechanisms or implementation details
-    - validation_ready: clear solution with target users or market intent
-
-    Then also return:
-    1. Ideation stage
-    2. Domain of solution
-    3. Subdomain of solution
-    4. One-line idea description
-    5. Short justification for choosing the ideation stage
-
-    Return ONLY valid JSON with exactly these keys:
-    {
-    "domain": "...",
-    "subdomain": "...",
-    "ideation_stage": "...",
-    "one_line_description": "...",
-    "justification": "..."
-    }
-    No markdown. No extra keys.
-    """
-    user_message = f"""User query: {query}
-    Confidence score (0 to 1): {confidence}
-    """
-    data=call_llm(prompt_template,user_message)
-    required = ["domain", "subdomain", "ideation_stage", "one_line_description","justification"]
-    return {k: data[k] for k in required}
 
 def calculate_confidence_score(doc):
     #Semantic focus
@@ -90,14 +52,81 @@ def calculate_confidence_score(doc):
     }
 
     confidence = (
-        scores["focus"] * 0.25 +
-        scores["product"] * 0.25 +
-        scores["constraints"] * 0.25 +
-        scores["noise"] * 0.25
+        scores["focus"] * 0.15 +
+        scores["product"] * 0.35 +
+        scores["constraints"] * 0.35 +
+        scores["noise"] * 0.15
         #scores["length"] * 0.2
     )
     
     return confidence
+
+def apply_user_edits(chat: dict, prev_understanding: dict, user_edit: str) -> dict:
+    prompt_template="""You are an expert product analyst.
+
+    You have a current structured understanding of a product idea.
+    The user may confirm it or provide corrections.
+
+    Rules:
+    - If the user message clearly confirms (yes/looks good/correct): return the SAME JSON unchanged.
+    - If the user message contains edits: update ONLY what the user changed.
+    - Keep all keys exactly the same.
+    - Return ONLY valid JSON. No markdown. No extra keys.
+    """
+
+    user_message=f"""Current JSON: {json.dumps(prev_understanding, ensure_ascii=False, indent=2)}
+    User message: {user_edit}
+    """
+    data, updated_contents=call_llm(prompt_template,user_message,chat.get("contents", []))
+    chat["contents"] = updated_contents
+
+    required = ["domain", "subdomain", "ideation_stage", "one_line_description","justification"]
+    new_understanding={k: data[k] for k in required}
+    
+    return new_understanding
+
+def idea_confirmation(chat, query, confidence):
+    prompt_template="""You are an expert product and startup analyst.
+    Given:
+    - A processed product idea: {query}
+    - A confidence score indicating specificity: {confidence}
+
+    Classify the ideation maturity of the idea using ONLY one of the following stages:
+    - exploration and problem_framing (initial_stage)
+    - solution_design and solution_detailing (intermediate_stage)
+    - market_validation_ready (final_stage)
+
+    Definitions:
+    - exploration and problem_framing: vague idea of a product, no much details
+    - solution_design and solution_detailing: a solution is proposed but not explained
+    - market_validation_ready: clear solution with target users and constraints
+
+    Then also return:
+    1. Ideation stage
+    2. Domain of solution
+    3. Subdomain of solution
+    4. One-line needs statement of the idea
+    5. Short justification for choosing the ideation stage
+
+    Return ONLY valid JSON with exactly these keys:
+    {
+    "domain": "...",
+    "subdomain": "...",
+    "ideation_stage": "...",
+    "one_line_description": "...",
+    "justification": "..."
+    }
+    No markdown. No extra keys.
+    """
+    user_message = f"""User query: {query}
+    Confidence score (0 to 1): {confidence}
+    """
+
+    data,updated_contents=call_llm(prompt_template,user_message)
+    chat["contents"] = updated_contents
+    
+    required = ["domain", "subdomain", "ideation_stage", "one_line_description","justification"]
+    return {k: data[k] for k in required}
 
 def preprocess_query(query):
     #Convert qury to lowercase and remove special characters
@@ -108,15 +137,52 @@ def preprocess_query(query):
     #Tokenization and lemmatization
     doc=nlp(query)
     processed_query=' '.join([tokens.lemma_ for tokens in doc if not tokens.is_stop])
-    print(processed_query)
     confidence_score=calculate_confidence_score(nlp(processed_query))
-    print("Score:",confidence_score)
+    print("Confidence Score:",confidence_score)
+    return processed_query, confidence_score
+    
+def chat_orchestration(user_id, query):
+    chat,chat_id=create_chat(user_id=user_id)
+    chat["idea_raw"]=query
+    add_turn(chat,"user",query)
 
-    response_dict=idea_confirmation(processed_query,confidence_score)
-    print(response_dict)
+    processed_query,confidence_score=preprocess_query(query)
+    
+    #Understanding the idea
+    print("\nPRODUCT IDEA DETAILING\n")
+    understanding=idea_confirmation(chat,processed_query,confidence_score)
+    chat["idea_understanding"]=understanding
+    chat["status"]="WAITING_IDEA_CONFIRMATION"
+    add_turn(chat, "assistant", "Extracted understanding:\n" + json.dumps(understanding, ensure_ascii=False, indent=2))
+    save_chat(chat)
+
+    #Confirm/edit
+    while True:
+        print("I understood your idea as:\n")
+        for k,v in understanding.items():
+            if k!="justification":
+                print(k+": "+v)
+        
+        user_reply= input("Is this correct? Confirm or write the changes you want to make: ")
+        if user_reply.lower().strip() in {"yes","y","correct", "looks good", "ok", "okay","confirm"}:
+            print("\nIdea confirmed\n")
+            chat["status"]="WAITING_CONSTRAINTS"
+            add_turn(chat, "assistant", "Idea confirmed. Moving to constraints.")
+            save_chat(chat)
+            break
+        else:
+            understanding=apply_user_edits(chat,understanding, user_reply)
+            chat["idea_understanding"] = understanding
+            add_turn(chat, "assistant", "Updated understanding:\n" + json.dumps(understanding, ensure_ascii=False, indent=2))
+            save_chat(chat)
+
+    #Constratints
+    print("\nConstraint analysis\n")
+    return chat_id
 
 #
 #query=input("Enter your idea: ")
-#query="I am building something very special. Low cost smartphones with a long battery life!!!"
-query="I am building something very cool. A low cost smartphones with a long battery life. This will be done by using a solar panel attached to the back of the phone."
-preprocess_query(query)
+query="I am building something very special. Low cost smartphones with a long battery life!!!"
+#query="I am building something very cool. A low cost smartphones with a long battery life. This will be done by using a solar panel attached to the back of the phone."
+#query="I am building something very cool. A smartphone in the range of Rs. 10000 with a battery life lasting 36 hours continuously. This will be done by using a solar panel attached to the back of the phone."
+chat_id=chat_orchestration(user_id="user_001", query=query)
