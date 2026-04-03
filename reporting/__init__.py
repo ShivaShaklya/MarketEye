7c79233983036437387422c8f85b9210a5cc2aed
Isha <ishushree19@gmail.com>
2026-04-03 11:37:45 +0530

    update report module

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -1,5 +1,3 @@
-"""Reporting utilities for MarketEye consulting-style PDF generation."""
-
 from .generator import ReportGenerator, load_input_payload, transform_chat_to_report_payload
 
 __all__ = ["ReportGenerator", "load_input_payload", "transform_chat_to_report_payload"]
